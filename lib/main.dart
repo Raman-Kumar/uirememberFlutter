c6960ff64cffea4456868e08736363df816e53c6
Raman-Kumar <k.raman1998@yahoo.in>
2020-02-12 19:35:49 +0530

    a sample list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+// import 'package:image';
 
 void main() => runApp(MyApp());
 
@@ -19,32 +21,93 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  int _ok = 0;
+
   @override
   Widget build(BuildContext context) {
+
+Future getImage() async {
+      var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+      setState(() {
+        // _image = image;
+          // print('Image Path $_image');
+      });
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("img/bluepic.jpg"),
-            fit: BoxFit.cover,
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("img/bluepic.jpg"),
+              fit: BoxFit.cover,
             ),
-         ),
-         child: Column(
-           mainAxisAlignment: MainAxisAlignment.start,
-           children: <Widget>[
-            Icon(Icons.add, size: 74.0,color: Colors.cyan,),
-           ],),
-      )
-    );
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Icon(
+                Icons.add,
+                size: 74.0,
+                color: Colors.cyan,
+              ),
+              new Text(
+                _counter.toString(),
+                style: TextStyle(color: Colors.teal[100],fontSize: 45.0),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  _counter++;
+                  setState(() {});
+                },
+                child: Text("Click Increse"),
+              ),
+              makeCard
+            ],
+          ),
+        ));
   }
+
+  final makeCard = Card(
+      elevation: 8.0,
+      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+      child: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+        child: makeListTile,
+      ),
+    );
+
+    static final makeListTile = ListTile(
+        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        leading: Container(
+          padding: EdgeInsets.only(right: 12.0),
+          decoration: new BoxDecoration(
+              border: new Border(
+                  right: new BorderSide(width: 1.0, color: Colors.white24))),
+          child: Icon(Icons.autorenew, color: Colors.white),
+        ),
+        title: Text(
+          "Introduction to Driving",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+        subtitle: Row(
+          children: <Widget>[
+            Icon(Icons.linear_scale, color: Colors.yellowAccent),
+            Text(" Intermediate", style: TextStyle(color: Colors.white))
+          ],
+        ),
+        trailing:
+            Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
 }
