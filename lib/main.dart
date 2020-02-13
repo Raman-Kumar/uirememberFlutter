5fa351ed6704b56cb5657bb36c775efe3633ab3c
Raman-Kumar <k.raman1998@yahoo.in>
2020-02-13 17:09:38 +0530

    A Card Like ListItemView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:math' as M;
+import 'dart:convert';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 // import 'package:image';
 
 void main() => runApp(MyApp());
@@ -31,15 +34,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
-Future getImage() async {
+    Future getImage() async {
       var image = await ImagePicker.pickImage(source: ImageSource.gallery);
 
       setState(() {
         // _image = image;
-          // print('Image Path $_image');
+        // print('Image Path $_image');
       });
     }
+
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
         appBar: AppBar(
@@ -63,7 +68,7 @@ Future getImage() async {
               ),
               new Text(
                 _counter.toString(),
-                style: TextStyle(color: Colors.teal[100],fontSize: 45.0),
+                style: TextStyle(color: Colors.teal[100], fontSize: 45.0),
               ),
               RaisedButton(
                 onPressed: () {
@@ -72,42 +77,113 @@ Future getImage() async {
                 },
                 child: Text("Click Increse"),
               ),
-              makeCard
+              makeCard,
+              buildaFoodCard(screenHeight, screenWidth),
             ],
           ),
         ));
   }
 
   final makeCard = Card(
-      elevation: 8.0,
-      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-      child: Container(
-        decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-        child: makeListTile,
+    elevation: 8.0,
+    margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+    child: Container(
+      decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+      child: makeListTile,
+    ),
+  );
+
+  static final makeListTile = ListTile(
+      contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      leading: Container(
+        padding: EdgeInsets.only(right: 12.0),
+        decoration: new BoxDecoration(
+            border: new Border(
+                right: new BorderSide(width: 1.0, color: Colors.white24))),
+        child: Icon(Icons.autorenew, color: Colors.white),
       ),
+      title: Text(
+        "Introduction to Driving",
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+      // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+      subtitle: Row(
+        children: <Widget>[
+          Icon(Icons.linear_scale, color: Colors.yellowAccent),
+          Text(" Intermediate", style: TextStyle(color: Colors.white))
+        ],
+      ),
+      trailing:
+          Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
+
+  InkWell buildaFoodCard(
+    screenHight,
+    screenWidth,
+  ) {
+    // I copied Card View Like Design from BannerWidgetArea class from
+    // Flutter Tutorial - Restaurant App UI Design | Flutter App
+    // https://www.youtube.com/watch?v=XgVSW2FYLKQ
+    return
+    InkWell(
+      onTap: (){
+        print("object");
+      },
+     child: Container(
+       margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+        width: screenWidth,
+        height: screenWidth * 9 / 16,
+        child: Container(
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black38,
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 5.0,
+                          spreadRadius: 1.0)
+                    ]),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                child: Image.asset(
+                  "img/butterfly.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.transparent, Colors.black])),
+              ),
+              Padding(
+                // padding: EdgeInsets.only(left:20,bottom: 10.0, right: 2.0),
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Food Name bannerItems[x]",
+                      style: TextStyle(fontSize: 25.0, color: Colors.white),
+                    ),
+                    Text(
+                      "More than 40% Off",
+                      style: TextStyle(fontSize: 12.0, color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ))
     );
-
-    static final makeListTile = ListTile(
-        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-        leading: Container(
-          padding: EdgeInsets.only(right: 12.0),
-          decoration: new BoxDecoration(
-              border: new Border(
-                  right: new BorderSide(width: 1.0, color: Colors.white24))),
-          child: Icon(Icons.autorenew, color: Colors.white),
-        ),
-        title: Text(
-          "Introduction to Driving",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
-
-        subtitle: Row(
-          children: <Widget>[
-            Icon(Icons.linear_scale, color: Colors.yellowAccent),
-            Text(" Intermediate", style: TextStyle(color: Colors.white))
-          ],
-        ),
-        trailing:
-            Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
+  }
 }
