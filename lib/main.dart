ab4d03eb88fbf984f46922935aae0af88f1bb558
Raman-Kumar <k.raman1998@yahoo.in>
2020-02-15 12:36:14 +0530

    a button to go to a page Shopping Page

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int _ok = 0;
-
+  static bool defaultPage = true;
+  static bool shoppingPage = false;
+  var screenWidth;
+  var screenHeight;
   @override
   Widget build(BuildContext context) {
     Future getImage() async {
@@ -43,45 +46,106 @@ class _MyHomePageState extends State<MyHomePage> {
       });
     }
 
-    var screenHeight = MediaQuery.of(context).size.height;
-    var screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("img/bluepic.jpg"),
-              fit: BoxFit.cover,
+    if (defaultPage)
+      return Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+          ),
+          body: Container(
+            // width: double.infinity,
+            height: screenHeight,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("img/bluepic.jpg"),
+                fit: BoxFit.cover,
+              ),
             ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Icon(
+                    Icons.add,
+                    size: 74.0,
+                    color: Colors.cyan,
+                  ),
+                  new Text(
+                    _counter.toString(),
+                    style: TextStyle(color: Colors.teal[100], fontSize: 45.0),
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      _counter++;
+                      defaultPage = true;
+                      setState(() {});
+                    },
+                    child: Text("Click Increse"),
+                  ),
+                  makeCard,
+                  buildaFoodCard(screenHeight, screenWidth),
+                    // making a Card Button to jump to new Page named Shopping page
+                  Card(
+                    elevation: 8.0,
+                    margin: new EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 6.0),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(64, 75, 96, .9)),
+                        child: ListTile(
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 10.0),
+                          leading: Container(
+                            padding: EdgeInsets.only(right: 12.0),
+                            decoration: new BoxDecoration(
+                                border: new Border(
+                                    right: new BorderSide(
+                                        width: 1.0, color: Colors.white24))),
+                            child:
+                                Icon(Icons.arrow_forward, color: Colors.white),
+                          ),
+                          title: Text(
+                            "Jump to shoping Cart page",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+                          subtitle: Row(
+                            children: <Widget>[
+                              Icon(Icons.shopping_basket,
+                                  color: Colors.yellowAccent),
+                              Text(" Explore a shopping sample",
+                                  style: TextStyle(color: Colors.white))
+                            ],
+                          ),
+                          onTap: () {
+                            print("ok");
+                            setState(() {
+                              shoppingPage = true;
+                              defaultPage = false;
+                            });
+                          },
+                        )),
+                  )
+                ],
+              ),
+            ),
+          ));
+
+// SHOPPING Page
+    if (shoppingPage) {
+      return Scaffold(
+          appBar: AppBar(
+            title: Text("Shopping Page"),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Icon(
-                Icons.add,
-                size: 74.0,
-                color: Colors.cyan,
-              ),
-              new Text(
-                _counter.toString(),
-                style: TextStyle(color: Colors.teal[100], fontSize: 45.0),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  _counter++;
-                  setState(() {});
-                },
-                child: Text("Click Increse"),
-              ),
-              makeCard,
-              buildaFoodCard(screenHeight, screenWidth),
-            ],
-          ),
-        ));
+          body: Container(
+            child :Text("I have made a seprate class for shopping change void main() => runApp(ShoppingApp())")
+          ));
+    }
   }
 
   final makeCard = Card(
@@ -124,66 +188,64 @@ class _MyHomePageState extends State<MyHomePage> {
     // I copied Card View Like Design from BannerWidgetArea class from
     // Flutter Tutorial - Restaurant App UI Design | Flutter App
     // https://www.youtube.com/watch?v=XgVSW2FYLKQ
-    return
-    InkWell(
-      onTap: (){
-        print("object");
-      },
-     child: Container(
-       margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-        width: screenWidth,
-        height: screenWidth * 9 / 16,
+    return InkWell(
+        onTap: () {
+          print("object");
+        },
         child: Container(
-          child: Stack(
-            fit: StackFit.expand,
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
+            margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+            width: screenWidth,
+            height: screenWidth * 9 / 16,
+            child: Container(
+              child: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black38,
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 5.0,
+                              spreadRadius: 1.0)
+                        ]),
+                  ),
+                  ClipRRect(
                     borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black38,
-                          offset: Offset(2.0, 2.0),
-                          blurRadius: 5.0,
-                          spreadRadius: 1.0)
-                    ]),
-              ),
-              ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                child: Image.asset(
-                  "img/butterfly.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [Colors.transparent, Colors.black])),
-              ),
-              Padding(
-                // padding: EdgeInsets.only(left:20,bottom: 10.0, right: 2.0),
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "Food Name bannerItems[x]",
-                      style: TextStyle(fontSize: 25.0, color: Colors.white),
+                    child: Image.asset(
+                      "img/butterfly.jpg",
+                      fit: BoxFit.cover,
                     ),
-                    Text(
-                      "More than 40% Off",
-                      style: TextStyle(fontSize: 12.0, color: Colors.white),
-                    )
-                  ],
-                ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [Colors.transparent, Colors.black])),
+                  ),
+                  Padding(
+                    // padding: EdgeInsets.only(left:20,bottom: 10.0, right: 2.0),
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Food Name bannerItems[x]",
+                          style: TextStyle(fontSize: 25.0, color: Colors.white),
+                        ),
+                        Text(
+                          "More than 40% Off",
+                          style: TextStyle(fontSize: 12.0, color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ))
-    );
+            )));
   }
 }
